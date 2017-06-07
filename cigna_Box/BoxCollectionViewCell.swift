7149da3e00db3430e37849095b8438126683f676
cccuser <cccuser@unameits-MacBook-Pro.local>
2017-06-07 20:43:32 +0530

    First commit

---
+++ cigna_Box/BoxCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  BoxCollectionViewCell.swift
+//  cigna_Box
+//
+//  Created by cccuser on 07/06/17.
+//  Copyright © 2017 cccuser. All rights reserved.
+//
+
+import UIKit
+
+class BoxCollectionViewCell: UICollectionViewCell {
+    
+  @IBOutlet weak var boxView: UIView!
+}
--- cigna_Box/ViewController.swift
+++ cigna_Box/ViewController.swift
@@ -2,24 +2,71 @@
 //  ViewController.swift
 //  cigna_Box
 //
-//  Created by cccuser on 07/06/17.
+//  Created by Rini Joseph on 07/06/17.
 //  Copyright © 2017 cccuser. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource{
+  @IBOutlet weak var boxCollectionView: UICollectionView!
+  @IBOutlet weak var countTextfield: UITextField!
+  var boxCount: Int = 0
 
+  @IBOutlet weak var submitButton: UIButton!
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+ 
+  @IBAction func submitAction(_ sender: Any) {
+    if countTextfield.text == "" {
+       boxCount = 0
+       boxCollectionView.reloadData()
+      let alertController = UIAlertController(title: "", message: "Count cannot be empty!!", preferredStyle: UIAlertControllerStyle.alert)
+      let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) {
+        (result : UIAlertAction) -> Void in
+        alertController.dismiss(animated: true, completion: nil)
+      }
+      alertController.addAction(okAction)
+      self.present(alertController, animated: true, completion: nil)
+    } else if Int(countTextfield.text ?? "")! > 10 {
+      boxCount = 0
+      boxCollectionView.reloadData()
+      countTextfield.text = ""
+      let alertController = UIAlertController(title: "", message: "Enter a count less than 10", preferredStyle: UIAlertControllerStyle.alert)
+      
+      let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) {
+        (result : UIAlertAction) -> Void in
+        alertController.dismiss(animated: true, completion: nil)
+      }
+      alertController.addAction(okAction)
+      self.present(alertController, animated: true, completion: nil)
+      
+    } else {
+      self.boxCount =  Int(countTextfield.text ?? "")!
+      boxCollectionView.reloadData()
+    }
+    
+  }
+  
+  // MARK: - UICollectionViewDataSource protocol
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return boxCount
   }
 
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BoxCell", for: indexPath as IndexPath) as! BoxCollectionViewCell
+    cell.isHidden = false
+    return cell
+  }
+  
+  // MARK: - UICollectionViewDelegate protocol
+  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    // handle tap events
+    print("You selected cell #\(indexPath.item)!")
+  }
 
 }
 
